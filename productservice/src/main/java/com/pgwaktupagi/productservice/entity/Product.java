0e02aac2c4049e10b235e5efd0828efe61718479
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-07-13 08:39:50 +0700

    asda

--- productservice/src/main/java/com/pgwaktupagi/productservice/entity/Product.java
+++ productservice/src/main/java/com/pgwaktupagi/productservice/entity/Product.java
@@ -25,8 +25,12 @@ public class Product {
 
     private String description;
 
+<<<<<<< HEAD
     private String images;
 
+=======
+    private String category;
+>>>>>>> 73ab693 (merge)
     @CreatedDate
     private LocalDateTime createdAt;
 
