98af37cda7eaf3093a136e4e8163b5f748d30734
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-08-01 14:09:50 +0700

    fix bug add to cart if two different product id

---
+++ cartservice/src/main/java/com/pgwaktupagi/cartservice/dto/ProductDTO.java
@@ -0,0 +1,72 @@
+package com.pgwaktupagi.productservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Schema(
+        name = "Products",
+        description = "Schema to hold Products information"
+)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+    @Schema(
+            description = "Id", example = "auto"
+    )
+    private String id;
+
+    @NotEmpty(message = "Product name cannot be null")
+    @Schema(description = "Product name", example = "Fried Chicken")
+    private String name;
+
+    @NotEmpty(message = "Product price cannot be null")
+    @Min(value = 0, message = "Product price must be greater than or equal to 0")
+    @Schema(description = "Product Price", example = "8000")
+    private Integer price;
+
+    @NotEmpty(message = "Product stock cannot be null")
+    @Min(value = 0, message = "Product stock must be greater than or equal to 0")
+    @Schema(description = "Product Stock", example = "10")
+    private Integer stock;
+
+    @NotNull(message = "Product description cannot be null")
+    @Schema(
+            description = "Product Description", example = "Fried Chicken is ,,,,"
+    )
+    private String description;
+
+    @NotEmpty(message = "Product category cannot be null")
+    @Schema(
+            description = "Product Category", example = "Makanan | Minuman"
+    )
+    private String category;
+
+    @Schema(
+            description = "Product image with extension jpg,jpeg,png", example = "food.jpg"
+    )
+    private String image;
+
+    @Schema(
+            hidden = true,
+            description = "Product image url", example = "http://localhost:8000/api/image/123e213213_avatar.png"
+    )
+    private String imageUrl;
+
+    @Schema(hidden = true)
+    private LocalDateTime createdAt;
+
+    @Schema(hidden = true)
+    private LocalDateTime updatedAt;
+}
---
+++ cartservice/src/main/java/com/pgwaktupagi/cartservice/service/client/ProductClient.java
@@ -0,0 +1,2 @@
+package com.pgwaktupagi.cartservice.service.client;public interface ProductClient {
+}
